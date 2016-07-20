c9c1c3507fbedbf2c9ef3d24c6e24df8bba821cb
Ben Norris <bennorris07@gmail.com>
2016-07-20 09:52:03 -0400

    fixed loop issues within ethical goods. now it exits properly.

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,7 +9,7 @@ class BuyNicely::CLI
 
 def prompt
   puts "Hi there. Would you like to check out 1. Ethically made goods or 2. American made goods?"
-  @input = gets.chomp
+  input = gets.chomp
   if input == "1"
     ethical_goods
   elsif input == "2"
@@ -22,60 +22,47 @@ end
 
 
 def ethical_goods
-  while @input != "exit"
-      puts "Great! How many different companies would you like to see? 5, 10, or 20? Type exit to leave."
-        answer = nil
-        while answer != "exit"
-          answer = gets.strip.downcase
-          case answer
-          when "5"
-            5.times {puts "Fair Trade Company"}
-            puts "What would you like to do now? See more, or exit?"
-            answer = gets.strip.downcase
-            if answer == "more"
-              show_more_ethical
-            elsif answer == "see more"
-              show_more_ethical
-            elsif answer == "exit"
-              @input = "exit"
-            end
-          when "10"
-            10.times {puts "Fair Trade Company"}
-            puts "What would you like to do now? See more, or exit?"
-            answer = gets.strip.downcase
-            if answer == "more"
-              show_more_ethical
-            elsif answer == "see more"
-              show_more_ethical
-            elsif answer == "exit"
-              @input = "exit"
-            end
-          when "20"
-            20.times {puts "Fair Trade Company"}
-            puts "What would you like to do now? See more, or exit?"
-            answer = gets.strip.downcase
-            if answer == "more"
-              show_more_ethical
-            elsif answer == "see more"
-              show_more_ethical
-            elsif answer == "exit"
-              @input = "exit"
-            end
-          when "more"
-            5.times {puts "Fair Trade Company"}
-            puts "What would you like to do now? See more, or exit?"
-            answer = gets.strip.downcase
-            if answer == "more"
-              show_more_ethical
-            elsif answer == "see more"
-              show_more_ethical
-            elsif answer == "exit"
-              @input = "exit"
-            end
-          end
+  input = nil
+  answer = nil
+  puts "Great! How many different companies would you like to see? 5, 10, or 20? Type exit to leave."
+    input = gets.strip.downcase
+      case input
+      when "5"
+        5.times {puts "Fair Trade Company"}
+        puts "What would you like to do now? See more, or exit?"
+        answer = gets.strip.downcase
+        if answer == "more"
+          ethical_goods
+        elsif answer == "see more"
+          ethical_goods
+        elsif answer == "exit"
+          input = "exit"
         end
-      end
+      when "10"
+        10.times {puts "Fair Trade Company"}
+        puts "What would you like to do now? See more, or exit?"
+        input = gets.strip.downcase
+        if input == "more"
+          ethical_goods
+        elsif input == "see more"
+          ethical_goods
+        elsif input == "exit"
+          answer = "exit"
+        end
+      when "20"
+        20.times {puts "Fair Trade Company"}
+        puts "What would you like to do now? See more, or exit?"
+        input = gets.strip.downcase
+        if input == "more"
+          ethical_goods
+        elsif input == "see more"
+          ethical_goods
+        elsif input == "exit"
+          anwser = "exit"
+        end
     end
+end
+
 
 def show_more_ethical
   ethical_goods
@@ -86,8 +73,8 @@ end
 
 def american_goods
   while @input != "exit"
-      puts "Excellent! How many different companies would you like to see? 5, 10, or 20? Type exit to leave."
-        answer = nil
+    puts "Excellent! How many different companies would you like to see? 5, 10, or 20? Type exit to leave."
+      answer = nil
         while answer != "exit"
           answer = gets.strip.downcase
           case answer
