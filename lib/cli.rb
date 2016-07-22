ce085fa38228cd600f16b5f5a5f0589a9cfee25b
Ben Norris <bennorris07@gmail.com>
2016-07-22 15:47:43 -0400

    edited loop on 'back' and 'more' for 5 random companies. need to do the same for 10

--- lib/cli.rb
+++ lib/cli.rb
@@ -21,6 +21,60 @@ def prompt
   end
 end
 
+def five_assorted_companies
+  @assorted_companies.shuffle![0..4].map.with_index(1) do |company,index|
+  puts "\n" + "#{index}. Company: #{company[:name]}\nCategory: #{company[:category]}"
+  end
+  five_assorted_companies_prompt
+end
+
+def five_assorted_companies_prompt
+  puts "\n" + "-Learn more about a company above by entering its number(1-5)."
+  puts "-See more companies by entering 'more'"
+  puts "-Go back to main menu by entering 'menu'"
+  puts "-Exit by entering 'exit'"
+  answer = gets.strip.downcase
+  case answer
+    when "1"
+      puts "Company: #{@assorted_companies[0][:name]}\nLocation: #{@assorted_companies[0][:location]}\nWebsite: #{@assorted_companies[0][:url]} "
+      see_more?(4)
+    when "2"
+      puts "Company: #{@assorted_companies[1][:name]}\nLocation: #{@assorted_companies[1][:location]}\nWebsite: #{@assorted_companies[1][:url]} "
+      see_more?(4)
+    when "3"
+      puts "Company: #{@assorted_companies[2][:name]}\nLocation: #{@assorted_companies[2][:location]}\nWebsite: #{@assorted_companies[2][:url]} "
+      see_more?(4)
+    when "4"
+      puts "Company: #{@assorted_companies[3][:name]}\nLocation: #{@assorted_companies[3][:location]}\nWebsite: #{@assorted_companies[3][:url]} "
+      see_more?(4)
+    when "5"
+      puts "Company: #{@assorted_companies[4][:name]}\nLocation: #{@assorted_companies[4][:location]}\nWebsite: #{@assorted_companies[4][:url]} "
+      see_more?(4)
+    when "more"
+      assorted_goods
+    when "menu"
+      prompt
+    when "exit"
+    end
+  end
+
+def see_more?(index)
+    puts "Would you like to 1. Go back 2. See more companies?"
+    input = gets.strip.downcase
+    case input
+    when "1" || "back"
+      @assorted_companies[0..index].map.with_index(1) do |company,index|
+      puts "\n" + "#{index}. Company: #{company[:name]}\nCategory: #{company[:category]}"
+      end
+      if index == 4
+        five_assorted_companies_prompt
+      elsif index == 9
+        ten_assorted_companies_prompt
+      end
+    when "2" || "see more"
+      assorted_goods
+    end
+  end
 
 def assorted_goods
   BuyNicely::AmericanList.assorted_scraper
@@ -31,39 +85,7 @@ def assorted_goods
     input = gets.strip.downcase
       case input
       when "5"
-        @assorted_companies.shuffle![0..4].map.with_index(1) do |company,index|
-        puts "\n" + "#{index}. Company: #{company[:name]}\nCategory: #{company[:category]}"
-        end
-
-        puts "\n" + "-Learn more about a company above by entering its number(1-5)."
-        puts "-See more companies by entering 'more'"
-        puts "-Go back to main menu by entering 'menu'"
-        puts "-Exit by entering 'exit'"
-        answer = gets.strip.downcase
-
-        case answer
-        when "1"
-          puts "Company: #{@assorted_companies[0][:name]}\nLocation: #{@assorted_companies[0][:location]}\nWebsite: #{@assorted_companies[0][:url]} "
-          see_more?
-        when "2"
-          puts "Company: #{@assorted_companies[1][:name]}\nLocation: #{@assorted_companies[1][:location]}\nWebsite: #{@assorted_companies[1][:url]} "
-          see_more?
-        when "3"
-          puts "Company: #{@assorted_companies[2][:name]}\nLocation: #{@assorted_companies[2][:location]}\nWebsite: #{@assorted_companies[2][:url]} "
-          see_more?
-        when "4"
-          puts "Company: #{@assorted_companies[3][:name]}\nLocation: #{@assorted_companies[3][:location]}\nWebsite: #{@assorted_companies[3][:url]} "
-          see_more?
-        when "5"
-          puts "Company: #{@assorted_companies[4][:name]}\nLocation: #{@assorted_companies[4][:location]}\nWebsite: #{@assorted_companies[4][:url]} "
-          see_more?
-        when "more"
-          assorted_goods
-        when "menu"
-          prompt
-        when "exit"
-        end
-
+        five_assorted_companies
       when "10"
         assorted_companies.shuffle[0..9].each do |company|
         puts "\n" + "Company: #{company[:name]}\nLocation: #{company[:location]}\nCategory: #{company[:category]}\nLink: #{company[:url]}"
@@ -142,19 +164,6 @@ def print_info(product_list)
     end
 end
 
-
-def see_more?
-  puts "Would you like to 1. Go back 2. See more companies?"
-  input = gets.strip.downcase
-  case input
-  when "1" || "back"
-    @assorted_companies[0..4].map.with_index(1) do |company,index|
-    puts "\n" + "#{index}. Company: #{company[:name]}\nCategory: #{company[:category]}"
-    end
-  when "2" || "see more"
-    assorted_goods
-  end
-end
 
 def category_selector
   puts "Which kind of companies would you like to see?"
