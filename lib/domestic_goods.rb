93d2b4eb46feac989895e7fa4a641deb6627edcf
Ben Norris <bennorris07@gmail.com>
2016-07-23 12:29:53 -0400

    updated required files

--- lib/domestic_goods.rb
+++ lib/domestic_goods.rb
@@ -2,6 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 
 
-require_relative "./domestic_goods/version"
-require_relative './cli'
+require_relative "./domestic_goods/version.rb"
+require_relative './cli.rb'
 require_relative "./domestic_goods/american_list.rb"
